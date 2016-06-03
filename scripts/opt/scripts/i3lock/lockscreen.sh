6b0092962efa374f4c004aab9448b8cca7f46e14
Rafael Nascimento <rafaeln.dev@gmail.com>
2016-06-03 12:12:16 -0300

    [Scripts]
    * Adicionado script para avisar sobre a discarda de bateria (cronjob).
    * Adicionado script get_containers_ip, retorna o ip dos containers do
    docker.
    * Corrigido i3exit para esperar o inicio do i3lock antes de
    hibernar/suspender.
    * Adicionado texto na lockscreen, e comentarios.
    * Adicionado script megaget_stream, para buscar arquivos de vídeo do
    MEGA utilizando o megatools, filtrar usando o fzf e realizar o stream
    por um vídeo player.
    * Adicionado scripts mudar_brilho e mudar_volume, utilizando o volnoti
    para mostrar uma notificação sobre a mudança.
    * Corrigido script open_firefox_itau
    * Modificado vlc_floating, utilizando smplayer (temporariamente)
    * Adicionado script zero_clock_mod_msr (teste de hibernação)

--- scripts/opt/scripts/i3lock/lockscreen.sh
+++ scripts/opt/scripts/i3lock/lockscreen.sh
@@ -3,8 +3,14 @@ ICON=/opt/scripts/i3lock/icon.png
 TMPBG=/tmp/screen.png
 scrot /tmp/screen.png
 #convert $TMPBG -scale 10% -scale 1000% $TMPBG
+
+TEXT="Digite a senha para desbloquear"
+# Cria a imagem com o efeito de blur
 convert $TMPBG -scale 10% -scale 1000% -fill black -colorize 75% $TMPBG
+# Coloca o ícone de chave no meio da imagem
 convert $TMPBG $ICON -gravity center -composite -matte $TMPBG
+# Adiciona um texto embaixo do ícone
+convert $TMPBG -fill white -pointsize 30 -gravity center -annotate +0+160 "$TEXT" $TMPBG
 
 /opt/scripts/playercontrol pause &
 i3lock -i $TMPBG
--- scripts/opt/scripts/vlc_floating.sh
+++ scripts/opt/scripts/vlc_floating.sh
@@ -2,14 +2,15 @@
 
 # Executar VLC
 # vlc --qt-minimal-view --no-fullscreen $1&
-mpv --ytdl-format=best $1&
+# mpv --ytdl-format=best $1&
+smplayer -no-fullscreen "$1"&
 
 # Esperar VLC abrir para reduzir o tamanho da janela
 while :; do
-  if [[ $(xdotool search -sync -all -onlyvisible --class mpv) ]]; then
-    # Modificar a janela do VLC para floating
+  if [[ $(xdotool search -sync -all -onlyvisible --class smplayer) ]]; then
+    # Modificar a janela do SMPlayer para floating
     # Mudar o tamanho da janela e mover para o canto do monitor
-    i3-msg '[class="mpv"] floating enable, sticky enable, resize set 400 200, move position 960 560'
+    i3-msg '[class="smplayer"] floating enable, sticky enable, resize set 400 200, move position 960 560'
     exit
   fi
 done
